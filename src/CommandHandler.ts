0d811fff45daa65831429dd0c28af92080b4f9ec
mmf39 <hockeybaseball112@gmail.com>
2026-07-19 23:15:08 -0400

    Refactor TEAM_LINEUP_SUBMITTER_USER_IDS for turkeys

--- src/CommandHandler.ts
+++ src/CommandHandler.ts
@@ -7,7 +7,8 @@ const CAP_LIMIT = 5000;
 const LINEUP_ADMIN_USER_IDS = ["Y3KdBmLn"];
 
 const TEAM_LINEUP_SUBMITTER_USER_IDS: Record<string, string[]> = {
-  turkeys: ["4JZo9wZv","R3XDLZz3"],
+  turkeys: ["4JZo9wZv"],
+  turkeys: ["R3XDLZz3"],
   gusnem: ["rner1dZJ"],
   thephantoms: ["5nxBPRyv"],
   illegals: ["5nxPZYQn"],
